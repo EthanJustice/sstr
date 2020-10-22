ab74550a3ba5bf8c10dbb5b8bca651fd4ca56fcc
EthanJustice <47954690+EthanJustice@users.noreply.github.com>
2020-10-22 18:34:26 -0400

    feat: basic cli arg parsing (clap)

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,20 @@
 // std
 
 // external
+use clap::{App, Arg, SubCommand};
 
 // local
 
 fn main() {
-    println!("Hello, world!");
+    let app = App::new("sstr")
+        .about(env!("CARGO_PKG_DESCRIPTION"))
+        .author(env!("CARGO_PKG_AUTHORS"))
+        .subcommand(
+            SubCommand::with_name("exec").about("Execute a task defined in the active workspace."),
+        )
+        .get_matches();
+
+    if let Some(_v) = app.subcommand_matches("exec") {
+        println!("Called exec, nothing implemented yet.");
+    }
 }
